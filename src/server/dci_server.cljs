3451d7054035f0dbc159e7702ad4202102e6bef8
gaberger <gary.berger@dell.com>
2019-04-24 19:05:20 +0000

    Addded tag filtering
    Signed-off-by: gaberger <gary.berger@dell.com>

--- src/server/dci_server.cljs
+++ src/server/dci_server.cljs
@@ -14,6 +14,7 @@
 (enable-console-print!)
 (defmulti command-actions identity)
 (defmethod command-actions :packet [& args]
+  (when (:debug @app-state) (println "command-actions" args))
   (condp = (second args)
     :list-servers (list-servers (PacketServer.) (nnext args))
     :get-project-name (get-project-name (PacketServer.) (nnext args))
@@ -26,33 +27,40 @@
     (println "calling create-handler" (count args))
     #_(dump-object (last args))))
 
+
+;TODO Make this common
+(defn get-project-details [parent cmd]
+ (if (some? (.-projectId cmd))
+   (let [project-id   (.-projectId cmd)
+         chan         (command-actions (keyword (.-provider parent))
+                                       :get-project-name project-id)
+         project-name (<! chan)]
+                      [project-id project-name])
+                    (utils/get-project-id-state)))
+
+;TODO Option does not deal with XOR
 (defn command-handler []
   (let [program (.. commander
                     (version "0.0.1")
                     (description "Server Module")
                     (option "-D --debug" "Debug")
                     (option "-J --json" "Output to JSON")
-                    (option "-P --provider <provider>" "Provider"  #"(?i)(packet|softlayer)$" "packet")
-                    )]
+                    (option "-E --edn" "Output to EDN")
+                    (option "-P --provider <provider>" "Provider"  #"(?i)(packet|softlayer)$" "packet"))]
 
     (if (utils/projectid?)
       (.. program
           (description "List Bare Metal Cloud Servers. Requires project-id")
           (command "list")
           (option "-p --project-id <project-id>" "Select Project")
+          (option "-T --tag <tag>" "Filter list by tag")
           (action (fn [cmd]
-                    (when (.-debug program) (swap! app-state assoc :debug true))
-                    (go
-                      (let [[id name] (if (some? (.-projectId cmd))
-                                        (let [project-id   (.-projectId cmd)
-                                              chan         (command-actions (keyword (.-provider program))
-                                                                            :get-project-name project-id)
-                                              project-name (<! chan)]
-                                          [project-id project-name])
-                                        (utils/get-project-id-state))]
-                        (when-not (:json @app-state)
-                          (println "Using Project:" name "\nID:" id))
-                        (command-actions (keyword (.-provider program)) :list-servers id))
+                     (go
+                       (let [tag       (if (.-tag cmd) {:filter (.-tag cmd)} {:filter []})
+                             [id name] (get-project-details program cmd)]
+                         (when (= (:output @app-state) :table)
+                                (println "Using Project:" name "\nID:" id))
+                              (command-actions (keyword (.-provider program)) :list-servers id tag))
                         ))))
 
     (.. program
@@ -67,27 +75,24 @@
          (.. program
              (command "create <hostname>")
              (option "-L --plan <plan>" "Select Plan")
-             (option "-F --facilities <facilities>" "Comman seperated list of facilities preferred facilities")
+             (option "-F --facilities <facilities>" "Comma seperated list of preferred facilities")
              (option "-O --os <os>" "Select Operating System")
              (option "-T --tags <tags>" "Comma seperated list of tags to apply to metadata")
              (description "Create Bare Metal Cloud Server.")
-             (action (fn [hostname program]
-                       (let [defaults       (.-parent program)
+             (action (fn [hostname cmd]
+                       (let [defaults       (.-parent cmd)
                              [project-id _] (utils/get-project-id-state)
                              hostname'      (str hostname)
-                             plan'          (or (.-plan program) "baremetal_0")
-                             tags'          (if (.-tags program) (str/split (.-tags program) #",") [])
-                             facilities'    (if (.-facilities program) (str/split (.-facilities program) #",") ["ewr1"] )
-                             os'            (if (.-os program) (str/split (.-os program) #",") "ubuntu_16_04")
+                             plan'          (or (.-plan cmd) "baremetal_0")
+                             tags'          (if (.-tags cmd) (str/split (.-tags program) #",") nil)
+                             facilities'    (if (.-facilities cmd) (str/split (.-facilities cmd) #",") ["ewr1"] )
+                             os'            (if (.-os cmd) (str/split (.-os cmd) #",") "ubuntu_16_04")
                              args           {:id               project-id
                                              :hostname         hostname'
                                              :plan             plan'
                                              :facility         facilities'
                                              :tags             tags'
                                              :operating_system os'}]
-                         (when (.-debug defaults) (do
-                                                    (println args)
-                                                    (swap! app-state assoc :debug true)))
                       (command-actions (keyword (.-provider defaults))
                                        :create-server args)))))
          (.. program
@@ -100,9 +105,7 @@
                             os'         (or os "ubuntu_16_04")
                             args        {:id       project-id  :hostname         hostname' :plan plan'
                                          :facility facilities' :operating_system os'}]
-                       (when (.-debug program) (swap! app-state assoc :debug true))
-                                         (command-actions (keyword (.-provider program))
-                                         :create-server args))))))
+                       (command-actions (keyword (.-provider program)) :create-server args))))))
 
     (.. program
         (command "delete <device-id>")
@@ -112,7 +115,18 @@
 
 
     (.parse program (.-argv js/process))
-    (when (.-json program) (swap! app-state assoc :json true))
+    (cond
+      (.-json program) (swap! app-state assoc :output :json)
+      (.-edn program)  (swap! app-state assoc :output :edn)
+      :else            (swap! app-state assoc :output :table)
+      )
+
+    (when (.-debug program) (do
+                                 (swap! app-state assoc :debug true)
+                                 (println "COMMANDER")
+                                 (js/console.log program)
+                                 (pprint/pprint @app-state)))
+
     (cond (= (.-args.length program) 0)
           (.. program 
               (help #(clojure.string/replace % #"dci-server" "server"))
